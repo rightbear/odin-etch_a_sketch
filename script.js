42cdf6afa6ab7943363241b524b3e14dea4f9244
rightbear <jjj17041113@gmail.com>
2025-01-14 00:25:23 +0800

    feat: design hover function on grid in JavaScript and CSS

--- script.js
+++ script.js
@@ -1,19 +1,35 @@
-
 const body = document.querySelector('body');
 
+// add grid cobtainer to include rows(consiisting of squares)
 const gridContainer = document.createElement('div');
 gridContainer.setAttribute("id", "container");
 
 body.appendChild(gridContainer);
 
 for (let i=1 ; i <= 16 ; i++){
+    // add rows in grid cobtainer to include squares
     let gridRow = document.createElement('div'); 
     gridRow.setAttribute("class", "rows");
     gridContainer.appendChild(gridRow);
 
+    // add squares in rows
     for (let j=1 ; j <=16 ; j++){
-        let gridElement = document.createElement('elements'); 
-        gridElement.setAttribute("class", "elements");
-        gridRow.appendChild(gridElement);
+        let gridSquare = document.createElement('div'); 
+        gridSquare.setAttribute("class", "squares");
+        gridRow.appendChild(gridSquare);
     }
 }
+
+// hover effect method1: add EventListner on element in class "squares" */
+const gridSquare = document.querySelectorAll(".squares");
+
+gridSquare.forEach((square) => {
+    square.addEventListener("mouseover", event => {
+        square.setAttribute("style", "background: red;");
+    });
+
+    square.addEventListener("mouseout", event => {
+        square.setAttribute("style", "background: white;");
+    });
+});
+  
